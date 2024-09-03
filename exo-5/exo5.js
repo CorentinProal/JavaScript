d16e931e1b6f3acce3b6faf034ddbc35ca5739f5
Corentin Proal <corentin.proal@Digital-Grenoble.local>
2024-09-03 09:33:33 +0200

    Exo 5

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,41 @@
 // you can write js here
 
-console.log('exo-5');
+console.log('Partie 5');
+
+/* Code avant changement étape 6
+let input = "Turpentine and turtles";
+let vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
+
+for (let letter of input) {
+    if (vowels.indexOf(letter.toLowerCase()) !== -1) {
+        resultArray.push(letter);
+    }
+}
+console.log(resultArray);
+ */
+
+// Code après changement étape 6
+let input = "Turpentine and turtles";
+let vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+    let letter = input[i];
+    let isVowel = false;
+    for (let j = 0; j < vowels.length; j++) {
+        if (letter.toLowerCase() === vowels[j]) {
+            isVowel = true;
+            break;
+        }
+    }
+    if (isVowel) {
+        resultArray.push(letter);
+    }
+}
+
+// Formatage du résultat
+let resultString = resultArray.join('').toUpperCase();
+
+console.log("Phrase Humain :", input);
+console.log("Trad  baleine :", resultString);
