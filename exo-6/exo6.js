028635a624557a7bf6335025dad68611d5cc413a
Corentin Proal <corentin.proal@Digital-Grenoble.local>
2024-09-03 11:19:43 +0200

    Exo 6

--- exo-6/exo6.js
+++ exo-6/exo6.js
@@ -11,7 +11,11 @@ var joeInfo = {
 // ! Ne faites pas de modification au dessus de cette ligne !
 
 // 1. Afficher le nombre de voitures de Joe
+console.log(joeInfo["cars"]);
 
 // 2. Changer le nombre de salles de bains de Joe : il n'en possède qu'une. Afficher le nouveau nombre de salles de bain.
-
+joeInfo.bathrooms = 1;
+console.log(joeInfo["bathrooms"]);
 // 3. Joe vient d'acquérir un garage. Afficher cette nouvelle information. 
+joeInfo.garage = true;
+console.log(joeInfo.garage);
