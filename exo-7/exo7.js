74aa1a83485d5ac30f2c703077c2d56b2dcff277
Corentin Proal <corentin.proal@Digital-Grenoble.local>
2024-09-04 15:20:50 +0200

    Partie 7 étape 3

--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -1,3 +1,24 @@
 console.log("Partie 7");
-
 console.log(jsonDatas);
+
+// Création de l'objet de traduction des types
+const tradType = {
+    car: "Voiture",
+    house: "Maison",
+    game: "Jeu",
+    videoGame: "Jeux Vidéo",
+    show: "Spectacle",
+};
+
+
+console.log(tradType);
+
+function affichage() {
+    for (let article of jsonDatas) {
+        const typeTraduit = tradType[article.type] || article.type;
+        article.typeTraduit = typeTraduit;
+        console.log(article.type, " Traduction :", article.typeTraduit);
+    }
+}
+
+affichage();
