0389bffa1e879e43b8ddf21a5b1b3ba5c7133957
CorentinProal <corentin.proal@le-campus-numerique.fr>
2024-09-05 12:02:29 +0200

    Delete JS-decouverte directory

--- JS-decouverte/exo-1/script.js
+++
@@ -1,6 +0,0 @@
-
-var kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?"); //
-let Celsius = kelvins - 273;
-let Fahrenheits = Celsius * (9/5) + 32
-console.log('Wsh la zone');
-console.log(Math.round(Fahrenheits));
