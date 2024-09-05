897393a4602992872cd738020f5b14fe3d313db1
Corentin Proal <corentin.proal@Digital-Grenoble.local>
2024-09-05 11:11:24 +0200

    Exo 7 filtrage sans trad

--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -1,9 +1,3 @@
-console.log("Partie 7");
-console.log(jsonDatas);
-
-
-
-////////////// Exo Trad /////////////////////////////////
 // // Création de l'objet de traduction des types
 // const tradType = {
 //     car: "Voiture",
@@ -27,23 +21,59 @@ console.log(jsonDatas);
 // affichage();
 
 
-function afficheType() {
-    const infoUl = document.getElementById('info');
+// function afficheType() {
+//     const infoUl = document.getElementById('info');
+//
+//     jsonDatas.forEach((article) => {
+//         const liElement = document.createElement('li');
+//         liElement.innerHTML = `
+//             Nom : ${article.name} <br>
+//             Type: ${article.type} <br>
+//             Description: ${article.description} <br>
+//             Price: ${article.price} <br>
+//             Quantity: ${article.quantity} <br><br>
+//         `;
+//         infoUl.appendChild(liElement);
+//     });
+//
+//     // Move this line inside the function
+//     Child(liElement);
+// }
+//
+// afficheType();
+
+
+
+console.log("Partie 7");
+console.log(jsonDatas);
+
+function afficheArticles(filtre = '') {
+    const listeArticles = document.getElementById('info');
+    listeArticles.innerHTML = '';
 
     jsonDatas.forEach((article) => {
-        const liElement = document.createElement('li');
-        liElement.innerHTML = `
-            Nom : ${article.name} <br>
-            Type: ${article.type} <br>
-            Description: ${article.description} <br>
-            Price: ${article.price} <br>
-            Quantity: ${article.quantity} <br><br>
-        `;
-        infoUl.appendChild(liElement);
+        if (filtre === '' || article.type.toLowerCase() === filtre.toLowerCase()) {
+            listeArticles.innerHTML += `
+                <li>
+                    Nom : ${article.name} <br>
+                    Type : ${article.type} <br>
+                    Prix : ${article.price} <br>
+                    Quantité : ${article.quantity} <br><br>
+                </li>
+            `;
+        }
     });
-
-    // Move this line inside the function
-    Child(liElement);
 }
 
-afficheType();
+const champSaisie = document.createElement('input');
+champSaisie.placeholder = 'Entrez le type d\'article';
+
+const boutonFiltre = document.createElement('button');
+boutonFiltre.textContent = 'Filtrer';
+boutonFiltre.onclick = () => afficheArticles(champSaisie.value);
+
+document.body.insertBefore(boutonFiltre, document.getElementById('info'));
+document.body.insertBefore(champSaisie, boutonFiltre);
+
+afficheArticles();
+
